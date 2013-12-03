540608316fe35dc465f57b8a8e49e9effcef3171
Alessandro Fanfarillo <elfanfa@ucar.edu>
2013-12-03 17:11:48 +0000

    New spmv testcase (matrix read from file)

--- test/kernel/compform.f90
+++ test/kernel/compform.f90
@@ -29,66 +29,63 @@
 !!$  POSSIBILITY OF SUCH DAMAGE.
 !!$ 
 !!$  
-! File: simple.f90
-!
-! Program: pdegenmv
-! This sample program measures the performance of the matrix-vector product.
-! The matrix is generated in the same way as for the pdegen test case of
-! the main PSBLAS library.
-!
-!
-program pdgenmv
+program compformat
   use psb_base_mod
   use psb_util_mod
+  use data_input
   use psb_ext_mod
   implicit none
 
   ! input parameters
-  character(len=5)  :: acfmt, agfmt
-  integer   :: idim
+  character(len=255) :: mtrx_file
 
-  ! miscellaneous 
-  real(psb_dpk_), parameter :: one = 1.d0
-  real(psb_dpk_) :: t1, t2, tprec, flops, tflops, tt1, tt2, gt1, gt2, gflops, bdwdth
+  ! sparse matrices
+  type(psb_dspmat_type) :: a, aux_a, agpu
 
-  ! sparse matrix and preconditioner
-  type(psb_dspmat_type) :: a, agpu
-  ! descriptor
-  type(psb_desc_type)   :: desc_a
   ! dense matrices
-  type(psb_d_vect_type) :: xv, bv, xg, bg 
-  !type(psb_d_vect_gpu)  :: vmold
+  real(psb_dpk_), allocatable, target :: aux_b(:,:), d(:)
+  real(psb_dpk_), allocatable , save  :: b_col(:), x_col(:), r_col(:), &
+       & x_col_glob(:), r_col_glob(:), bres(:)
+  real(psb_dpk_), pointer  :: b_col_glob(:)
+  type(psb_d_vect_type) :: xg, bg, xv, bv
+!  type(psb_d_vect_gpu)  :: vmold
   real(psb_dpk_), allocatable :: xc1(:),xc2(:)
-  ! blacs parameters
-  integer            :: ictxt, iam, np
+  ! communications data structure
+  type(psb_desc_type):: desc_a
 
-  ! solver parameters
+  integer            :: ictxt, iam, np
   integer(psb_long_int_k_) :: amatsize, precsize, descsize, annz, nbytes
-  real(psb_dpk_)   :: err, eps
+  real(psb_dpk_)   :: err, eps 
+
+  character(len=5)   :: afmt
+  character(len=20)  :: name
+  character(len=2)   :: filefmt
+  integer, parameter :: iunit=12
+  integer, parameter :: times=2000 
   integer, parameter :: ntests=200, ngpu=50 
+
   type(psb_d_csr_sparse_mat), target   :: acsr
-  type(psb_d_hdia_sparse_mat), target   :: ahdia
-  type(psb_d_dia_sparse_mat), target   :: adia
   type(psb_d_ell_sparse_mat), target   :: aell
   type(psb_d_rsb_sparse_mat), target   :: arsb
-  !type(psb_d_elg_sparse_mat), target   :: aelg
-  !type(psb_d_csrg_sparse_mat), target  :: acsrg
-  !type(psb_d_hybg_sparse_mat), target  :: ahybg
+  type(psb_d_hdia_sparse_mat), target   :: ahdia
+  type(psb_d_dia_sparse_mat), target   :: adia
+  ! type(psb_d_elg_sparse_mat), target   :: aelg
+  ! type(psb_d_csrg_sparse_mat), target  :: acsrg
+  ! type(psb_d_hybg_sparse_mat), target  :: ahybg
   type(psb_d_hll_sparse_mat), target   :: ahll
-  !type(psb_d_hlg_sparse_mat), target   :: ahlg
-  class(psb_d_base_sparse_mat), pointer :: agmold, acmold
+  ! type(psb_d_hlg_sparse_mat), target   :: ahlg
+  class(psb_d_base_sparse_mat), pointer :: amold
   ! other variables
-  logical, parameter :: dump=.true.
-  integer            :: info, i, nr
-  character(len=20)  :: name,ch_err
-  character(len=40)  :: fname
-
-  info=psb_success_
+  integer            :: i,info,j,nrt, ns, nr, ipart
+  integer            :: internal, m,ii,nnzero
+  real(psb_dpk_) :: t1, t2, tprec, flops
+  real(psb_dpk_) :: tt1, tt2, tflops, gt1, gt2,gflops, gtint, bdwdth
+  integer :: nrhs, nrow, n_row, dim, nv, ne
+  integer, allocatable :: ivg(:), ipv(:)
 
 
   call psb_init(ictxt)
   call psb_info(ictxt,iam,np)
-
   call psb_rsb_init()
 
   if (iam < 0) then 
@@ -96,69 +93,148 @@ program pdgenmv
     call psb_exit(ictxt)
     stop
   endif
+
+
+  name='file_spmv'
   if(psb_get_errstatus() /= 0) goto 9999
-  name='pde90'
+  info=psb_success_  
   call psb_set_errverbosity(2)
-  !
-  ! Hello world
-  !
-  if (iam == psb_root_) then 
-    write(*,*) 'Welcome to PSBLAS version: ',psb_version_string_
-    write(*,*) 'This is the ',trim(name),' sample program'
+
+  if (iam == 0) then
+     write(*,*) 'path-fileformat(MM/HB)-matrixformat(CSR/ELL/ecc..)-partition(0:block,2:graph)'
+     call read_data(mtrx_file,psb_inp_unit)
+     call read_data(filefmt,psb_inp_unit)
+     call read_data(afmt,psb_inp_unit)
+     call read_data(ipart,psb_inp_unit)
   end if
-  !
-  !  get parameters
-  !
-  call get_parms(ictxt,acfmt,idim)
+  call psb_bcast(ictxt,mtrx_file)
+  call psb_bcast(ictxt,filefmt)
+  call psb_bcast(ictxt,afmt)
+  call psb_bcast(ictxt,ipart)
+  call psb_barrier(ictxt)
+  t1 = psb_wtime()  
+  ! read the input matrix to be processed and (possibly) the rhs 
+  nrhs = 1
 
-  !
-  !  allocate and fill in the coefficient matrix and initial vectors
-  !
-  call psb_barrier(ictxt)
-  t1 = psb_wtime()
-  call psb_gen_pde3d(ictxt,idim,a,bv,xv,desc_a,'CSR  ',&
-       & a1,a2,a3,b1,b2,b3,c,g,info)  
-  call psb_barrier(ictxt)
-  t2 = psb_wtime() - t1
-  if(info /= psb_success_) then
-    info=psb_err_from_subroutine_
-    ch_err='create_matrix'
-    call psb_errpush(info,name,a_err=ch_err)
-    goto 9999
+  if (iam==psb_root_) then
+    select case(psb_toupper(filefmt)) 
+    case('MM') 
+      ! For Matrix Market we have an input file for the matrix
+      ! and an (optional) second file for the RHS. 
+      call mm_mat_read(aux_a,info,iunit=iunit,filename=mtrx_file)
+
+    case ('HB')
+      ! For Harwell-Boeing we have a single file which may or may not
+      ! contain an RHS.
+      call hb_read(aux_a,info,iunit=iunit,filename=mtrx_file)
+
+    case default
+      info = -1 
+      write(psb_err_unit,*) 'Wrong choice for fileformat ', filefmt
+    end select
+    if (info /= 0) then
+      write(psb_err_unit,*) 'Error while reading input matrix '
+      call psb_abort(ictxt)
+    end if
+
+    nrt = aux_a%get_nrows()
+    call psb_bcast(ictxt,nrt)
+
+    write(psb_out_unit,'("Generating an rhs...")')
+    write(psb_out_unit,'(" ")')
+    call psb_realloc(nrt,1,aux_b,info)
+    if (info /= 0) then
+      call psb_errpush(4000,name)
+      goto 9999
+    endif
+
+    b_col_glob => aux_b(:,1)
+    do i=1, nrt
+      b_col_glob(i) = 1.d0
+    enddo
+    call psb_bcast(ictxt,b_col_glob(1:nrt))
+
+  else
+
+    call psb_bcast(ictxt,nrt)
+    call psb_realloc(nrt,1,aux_b,info)
+    if (info /= 0) then
+      call psb_errpush(4000,name)
+      goto 9999
+    endif
+    b_col_glob =>aux_b(:,1)
+    call psb_bcast(ictxt,b_col_glob(1:nrt)) 
+
   end if
-  if (iam == psb_root_) write(psb_out_unit,'("Overall matrix creation time : ",es12.5)')t2
-  if (iam == psb_root_) write(psb_out_unit,'(" ")')
 
-  ! if (dump) then 
-  !   write(fname,'(a,i3.3,a)') 'pde',idim,'.mtx'
-  !   call a%print(fname,head='PDEGEN test matrix')
-  ! end if
 
-  select case(psb_toupper(acfmt))
-  case('ELL')
-    acmold => aell
-  case('HLL')
-    acmold => ahll
+  select case(psb_toupper(afmt))
   case('CSR')
-    acmold => acsr
+     amold => acsr
+  case('ELL')
+     amold => aell
+  case('HLL')
+     amold => ahll
   case('DIA')
-    acmold => adia
+     amold => adia
   case('HDIA')
-    acmold => ahdia
+     amold => ahdia
   case('RSB')
-    acmold => arsb
+     amold => arsb
   case default
-    write(*,*) 'Unknown format defaulting to HLL'
-    acmold => ahll
+     write(*,*) 'Unknown format defaulting to HLG'
+     amold => acsr
   end select
-  call a%cscnv(info,mold=acmold)
-  if ((info /= 0).or.(psb_get_errstatus()/=0)) then 
-    write(0,*) 'From cscnv ',info
-    call psb_error()
-    stop
+
+  ! switch over different partition types
+  if (ipart == 0) then 
+    call psb_barrier(ictxt)
+    if (iam==psb_root_) write(psb_out_unit,'("Partition type: block")')
+    allocate(ivg(nrt),ipv(np))
+    do i=1,nrt
+      call part_block(i,nrt,np,ipv,nv)
+      ivg(i) = ipv(1)
+    enddo
+    call psb_matdist(aux_a, a, ictxt, &
+         & desc_a,b_col_glob,bv,info,v=ivg)
+  else if (ipart == 2) then 
+    if (iam==psb_root_) then 
+      write(psb_out_unit,'("Partition type: graph")')
+      write(psb_out_unit,'(" ")')
+      !      write(psb_err_unit,'("Build type: graph")')
+      call build_mtpart(aux_a,np)
+    endif
+    call psb_barrier(ictxt)
+    call distr_mtpart(psb_root_,ictxt)
+    call getv_mtpart(ivg)
+    call psb_matdist(aux_a, a, ictxt, &
+         & desc_a,b_col_glob,bv,info,v=ivg)
+  else 
+    if (iam==psb_root_) write(psb_out_unit,'("Partition type default: block")')
+    call psb_matdist(aux_a, a,  ictxt, &
+         & desc_a,b_col_glob,bv,info,parts=part_block)
   end if
 
-  call xv%set(done)
+  call psb_geall(x_col,desc_a,info)
+  ns = size(x_col)
+  do i=1, ns
+    x_col(i) = 1.0 + (1.0*i)/ns
+  end do
+  call psb_geasb(x_col,desc_a,info)
+  t2 = psb_wtime() - t1
+
+  !call a%cscnv(agpu,info,mold=amold)
+call xv%bld(x_col)
+ ! call xg%bld(x_col,mold=vmold)
+ call psb_geasb(bv,desc_a,info,scratch=.true.)
+!  call psb_geasb(bg,desc_a,info,scratch=.true.,mold=vmold)
+ call psb_amx(ictxt, t2)
+
+  if (iam==psb_root_) then
+    write(psb_out_unit,'(" ")')
+    write(psb_out_unit,'("Time to read and partition matrix : ",es12.5)')t2
+    write(psb_out_unit,'(" ")')
+  end if
 
   call psb_barrier(ictxt)
   t1 = psb_wtime()
@@ -169,16 +245,11 @@ program pdgenmv
   t2 = psb_wtime() - t1
   call psb_amx(ictxt,t2)
 
+  ! FIXME: cache flush needed here
   call psb_barrier(ictxt)
-  
-  call psb_rsb_exit()
-
-  ! ! FIXME: cache flush needed here
-  ! call psb_barrier(ictxt)
-  ! tt1 = psb_wtime()
+  tt1 = psb_wtime()
   ! do i=1,ntests 
   !   call psb_spmm(done,agpu,xv,dzero,bg,desc_a,info)
-  !   call psb_gpu_DeviceSync()
   !   if ((info /= 0).or.(psb_get_errstatus()/=0)) then 
   !     write(0,*) 'From 1 spmm',info,i,ntests
   !     call psb_error()
@@ -186,22 +257,22 @@ program pdgenmv
   !   end if
 
   ! end do
-  ! call psb_barrier(ictxt)
-  ! tt2 = psb_wtime() - tt1
-  ! call psb_amx(ictxt,tt2)
-  ! xc1 = bv%get_vect()
-  ! xc2 = bg%get_vect()
-  ! nr       = desc_a%get_local_rows() 
-  ! eps = maxval(abs(xc1(1:nr)-xc2(1:nr)))
-  ! call psb_amx(ictxt,eps)
-  ! if (iam==0) write(*,*) 'Max diff on xGPU',eps
+ call psb_barrier(ictxt)
+  tt2 = psb_wtime() - tt1
+! call psb_amx(ictxt,tt2)
+! xc1 = bv%get_vect()
+! xc2 = bg%get_vect()
+  nr       = desc_a%get_local_rows() 
+! eps = maxval(abs(xc1(1:nr)-xc2(1:nr)))
+! call psb_amx(ictxt,eps)
+! if (iam==0) write(*,*) 'Max diff on xGPU',eps
 
 
-  ! call xg%sync()
-  ! ! FIXME: cache flush needed here
+ !call xg%sync()
+  ! FIXME: cache flush needed here
 
-  ! call psb_barrier(ictxt)
-  ! gt1 = psb_wtime()
+  call psb_barrier(ictxt)
+  gt1 = psb_wtime()
   ! do i=1,ntests*ngpu
   !   ! Make sure the X vector is on the GPU side of things.
   !   select type (v => xg%v)
@@ -217,22 +288,21 @@ program pdgenmv
   !     call psb_error()
   !     stop
   !   end if
-
   ! end do
-  ! call psb_barrier(ictxt)
-  ! gt2 = psb_wtime() - gt1
-  ! call psb_amx(ictxt,gt2)
-  ! call bg%sync()
-  ! xc1 = bv%get_vect()
-  ! xc2 = bg%get_vect()
-  ! call psb_geaxpby(-done,bg,+done,bv,desc_a,info)
-  ! eps = psb_geamax(bv,desc_a,info)
+  call psb_barrier(ictxt)
+  gt2 = psb_wtime() - gt1
+ !call psb_amx(ictxt,gt2)
+ !call bg%sync()
+ !xc1 = bv%get_vect()
+ !xc2 = bg%get_vect()
+ !call psb_geaxpby(-done,bg,+done,bv,desc_a,info)
+ !eps = psb_geamax(bv,desc_a,info)
 
-  ! call psb_amx(ictxt,t2)
+ ! call psb_amx(ictxt,t2)
   nr       = desc_a%get_local_rows() 
-  ! eps = maxval(abs(xc1(1:nr)-xc2(1:nr)))
-  ! call psb_amx(ictxt,eps)
-  ! if (iam==0) write(*,*) 'Max diff on GPU',eps
+ !eps = maxval(abs(xc1(1:nr)-xc2(1:nr)))
+ !call psb_amx(ictxt,eps)
+ !if (iam==0) write(*,*) 'Max diff on GPU',eps
 
   annz     = a%get_nzeros()
   amatsize = a%sizeof()
@@ -242,12 +312,11 @@ program pdgenmv
   call psb_sum(ictxt,descsize)
 
   if (iam == psb_root_) then
-    write(psb_out_unit,&
-         & '("Matrix: ell1 ",i0)') idim
+    write(psb_out_unit,'("Matrix: ",a)') mtrx_file
     write(psb_out_unit,&
          &'("Test on                          : ",i20," processors")') np
     write(psb_out_unit,&
-         &'("Size of matrix                   : ",i20,"           ")') nr
+         &'("Size of matrix                   : ",i20,"           ")') nr!t
     write(psb_out_unit,&
          &'("Number of nonzeros               : ",i20,"           ")') annz
     write(psb_out_unit,&
@@ -259,7 +328,7 @@ program pdgenmv
     tflops = tflops / (tt2)
     gflops = gflops / (gt2)
     write(psb_out_unit,'("Storage type for    A: ",a)') a%get_fmt()
-    write(psb_out_unit,'("Storage type for AGPU: ",a)') agpu%get_fmt()
+ !  write(psb_out_unit,'("Storage type for AGPU: ",a)') agpu%get_fmt()
     write(psb_out_unit,&
          & '("Number of flops (",i0," prod)        : ",F20.0,"           ")') &
          &  ntests,flops
@@ -300,113 +369,21 @@ program pdgenmv
 
   end if
 
-  !  
-  !  cleanup storage and exit
-  !
-  call psb_gefree(bv,desc_a,info)
-  call psb_gefree(xv,desc_a,info)
-  call psb_spfree(a,desc_a,info)
+  call psb_gefree(b_col, desc_a,info)
+  call psb_gefree(x_col, desc_a,info)
+  call psb_gefree(xv, desc_a,info)
+  call psb_gefree(bv, desc_a,info)
+  call psb_gefree(xg, desc_a,info)
+  call psb_gefree(bg, desc_a,info)
+  call psb_spfree(a, desc_a,info)
+  call psb_spfree(agpu,desc_a,info)
   call psb_cdfree(desc_a,info)
-  if(info /= psb_success_) then
-    info=psb_err_from_subroutine_
-    ch_err='free routine'
-    call psb_errpush(info,name,a_err=ch_err)
-    goto 9999
-  end if
 
 9999 continue
-  if(info /= psb_success_) then
+  if(info /= 0) then
     call psb_error(ictxt)
   end if
   call psb_exit(ictxt)
   stop
 
-contains
-  !
-  ! get iteration parameters from standard input
-  !
-  subroutine  get_parms(ictxt,acfmt,idim)
-    integer      :: ictxt
-    character(len=*) :: acfmt
-    integer      :: idim
-    integer      :: np, iam
-    integer      :: intbuf(10), ip
-
-    call psb_info(ictxt, iam, np)
-
-    if (iam == 0) then
-      read(psb_inp_unit,*) acfmt
-      read(psb_inp_unit,*) idim
-    endif
-    call psb_bcast(ictxt,acfmt)
-    call psb_bcast(ictxt,idim)
-
-    if (iam == 0) then
-      write(psb_out_unit,'("Testing matrix       : ell1")')      
-      write(psb_out_unit,'("Grid dimensions      : ",i4,"x",i4,"x",i4)')idim,idim,idim
-      write(psb_out_unit,'("Number of processors : ",i0)')np
-      write(psb_out_unit,'("Data distribution    : BLOCK")')
-      write(psb_out_unit,'(" ")')
-    end if
-    return
-
-  end subroutine get_parms
-
-  !
-  ! functions parametrizing the differential equation 
-  !  
-  function a1(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) :: a1
-    real(psb_dpk_), intent(in) :: x,y,z
-    a1=1.d0
-  end function a1
-  function a2(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) ::  a2
-    real(psb_dpk_), intent(in) :: x,y,z
-    a2=2.d1*y
-  end function a2
-  function a3(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) ::  a3
-    real(psb_dpk_), intent(in) :: x,y,z      
-    a3=1.d0
-  end function a3
-  function c(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) :: c
-    real(psb_dpk_), intent(in) :: x,y,z      
-    c=1.d0
-  end function c
-  function b1(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) ::  b1   
-    real(psb_dpk_), intent(in) :: x,y,z
-    b1=1.d0
-  end function b1
-  function b2(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) ::  b2
-    real(psb_dpk_), intent(in) :: x,y,z
-    b2=1.d0
-  end function b2
-  function b3(x,y,z)
-    use psb_base_mod, only : psb_dpk_
-    real(psb_dpk_) ::  b3
-    real(psb_dpk_), intent(in) :: x,y,z
-    b3=1.d0
-  end function b3
-  function g(x,y,z)
-    use psb_base_mod, only : psb_dpk_, done, dzero
-    real(psb_dpk_) ::  g
-    real(psb_dpk_), intent(in) :: x,y,z
-    g = dzero
-    if (x == done) then
-      g = done
-    else if (x == dzero) then 
-      g = exp(y**2-z**2)
-    end if
-  end function g
-
-end program pdgenmv
+end program compformat
