306ad3bb19530b533675d1e536611e631e289ba2
Salvatore Filippone <salvatore.filippone@cranfield.ac.uk>
2014-10-22 12:09:43 +0000

    psblas3-ext:  ext/fileread/Makefile  ext/fileread/data_input.f90  ext/fileread/df_sample.F90  ext/fileread/runs/dfs.inp  ext/kernel/Makefile  ext/kernel/c_file_spmv.F90  ext/kernel/d_file_spmv.F90  ext/kernel/data_input.f90  ext/kernel/dpdegenmv.F90  ext/kernel/runs/dfs.inp  ext/kernel/s_file_spmv.F90  ext/kernel/spdegenmv.F90  ext/kernel/z_file_spmv.F90  ext/pdegen/Makefile  ext/pdegen/doppde.sh  ext/pdegen/mldpde.f90  ext/pdegen/mldpde2d.f90  ext/pdegen/mmldpde.f90  ext/pdegen/pde2d_base_mod.f90  ext/pdegen/pde2d_box_mod.f90  ext/pdegen/pde2d_exp_mod.f90  ext/pdegen/pde2d_gauss_mod.f90  ext/pdegen/pde3d_base_mod.f90  ext/pdegen/pde3d_exp_mod.f90  ext/pdegen/pde3d_gauss_mod.f90  ext/pdegen/ppde.f90  ext/pdegen/ppde2d.f90  ext/pdegen/runs/mld.inp  ext/pdegen/runs/ppde.inp  fileread/Makefile  fileread/data_input.f90  fileread/df_sample.F90  fileread/runs/dfs.inp  gpu/fileread/Makefile  gpu/fileread/data_input.f90  gpu/fileread/df_sample.F90  gpu/fileread/runs/dfs.inp  gpu/kernel/Makefile  gpu/kernel/c_file_spmv.F90  gpu/kernel/d_file_spmv.F90  gpu/kernel/data_input.f90  gpu/kernel/dpdegenmv.F90  gpu/kernel/runs/dfs.inp  gpu/kernel/s_file_spmv.F90  gpu/kernel/spdegenmv.F90  gpu/kernel/z_file_spmv.F90  gpu/pdegen/Makefile  gpu/pdegen/doppde.sh  gpu/pdegen/mldpde.f90  gpu/pdegen/mldpde2d.f90  gpu/pdegen/mmldpde.f90  gpu/pdegen/pde2d_base_mod.f90  gpu/pdegen/pde2d_box_mod.f90  gpu/pdegen/pde2d_exp_mod.f90  gpu/pdegen/pde2d_gauss_mod.f90  gpu/pdegen/pde3d_base_mod.f90  gpu/pdegen/pde3d_exp_mod.f90  gpu/pdegen/pde3d_gauss_mod.f90  gpu/pdegen/ppde.f90  gpu/pdegen/ppde2d.f90  gpu/pdegen/runs/mld.inp  gpu/pdegen/runs/ppde.inp  kernel/Makefile  kernel/c_file_spmv.F90  kernel/d_file_spmv.F90  kernel/data_input.f90  kernel/dpdegenmv.F90  kernel/runs/dfs.inp  kernel/s_file_spmv.F90  kernel/spdegenmv.F90  kernel/z_file_spmv.F90  pdegen/Makefile  pdegen/doppde.sh  pdegen/mldpde.f90  pdegen/mldpde2d.f90  pdegen/mmldpde.f90  pdegen/pde2d_base_mod.f90  pdegen/pde2d_box_mod.f90  pdegen/pde2d_exp_mod.f90  pdegen/pde2d_gauss_mod.f90  pdegen/pde3d_base_mod.f90  pdegen/pde3d_exp_mod.f90  pdegen/pde3d_gauss_mod.f90  pdegen/ppde.f90  pdegen/ppde2d.f90  pdegen/runs/mld.inp  pdegen/runs/ppde.inp
    Reorg test dir, step 2.

---
+++ test/gpu/pdegen/pde2d_gauss_mod.f90
@@ -0,0 +1,54 @@
+module pde2d_gauss_mod
+  use psb_base_mod, only : psb_dpk_
+  real(psb_dpk_), save, private :: epsilon=1.d0/80
+contains
+  subroutine pde2d_set_parm(dat)
+    real(psb_dpk_), intent(in) :: dat
+    epsilon = dat
+  end subroutine pde2d_set_parm
+  !
+  ! functions parametrizing the differential equation 
+  !  
+  function b1(x,y)
+    use psb_base_mod, only : psb_dpk_
+    real(psb_dpk_) :: b1
+    real(psb_dpk_), intent(in) :: x,y
+    b1 = 1.d0/1.414d0-2*x*exp(-(x**2+y**2))
+  end function b1
+  function b2(x,y)
+    use psb_base_mod, only : psb_dpk_
+    real(psb_dpk_) ::  b2
+    real(psb_dpk_), intent(in) :: x,y
+    b2 = 1.d0/1.414d0-2*y*exp(-(x**2+y**2))
+  end function b2
+  function c(x,y)
+    use psb_base_mod, only : psb_dpk_
+    real(psb_dpk_) ::  c
+    real(psb_dpk_), intent(in) :: x,y      
+    c = 0.d0 
+  end function c
+  function a1(x,y)
+    use psb_base_mod, only : psb_dpk_
+    real(psb_dpk_) ::  a1   
+    real(psb_dpk_), intent(in) :: x,y
+    a1=1.d0*epsilon*exp(-(x**2+y**2))
+  end function a1
+  function a2(x,y)
+    use psb_base_mod, only : psb_dpk_
+    real(psb_dpk_) ::  a2
+    real(psb_dpk_), intent(in) :: x,y
+    a2=1.d0*epsilon*exp(-(x**2+y**2))
+  end function a2
+  function g(x,y)
+    use psb_base_mod, only : psb_dpk_, done, dzero
+    real(psb_dpk_) ::  g
+    real(psb_dpk_), intent(in) :: x,y
+    g = dzero
+    if (x == done) then
+      g = done
+    else if (x == dzero) then 
+      g = done
+    end if
+  end function g
+end module pde2d_gauss_mod
+
